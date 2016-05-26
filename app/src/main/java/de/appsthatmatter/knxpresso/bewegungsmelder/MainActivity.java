a7499324058cc3371e4256a8dd2d419146e32821
jogehring <jogehring@inovex.de>
2016-05-26 19:25:36 +0200

    perfekt

--- app/src/main/java/de/appsthatmatter/knxpresso/bewegungsmelder/MainActivity.java
+++ app/src/main/java/de/appsthatmatter/knxpresso/bewegungsmelder/MainActivity.java
@@ -17,9 +17,6 @@ import de.appsthatmatter.knxpresso.bewegungsmelder.motiondetection.MotionDetecto
 
 public class MainActivity extends AppCompatActivity {
     private TextView txtStatus;
-    private Camera mCamera;
-    private boolean inPreview;
-    private SurfaceHolder previewHolder;
     private MotionDetector motionDetector;
 
 
@@ -30,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
         txtStatus = (TextView) findViewById(R.id.txtStatus);
 
-        motionDetector = new MotionDetector();
+        motionDetector = new MotionDetector(this, (SurfaceView) findViewById(R.id.surfaceView));
         motionDetector.setMotionDetectorCallback(new MotionDetectorCallback() {
             @Override
             public void onMotionDetected() {
@@ -44,7 +41,6 @@ public class MainActivity extends AppCompatActivity {
                 txtStatus.setText("Zu dunkel hier");
             }
         });
-        motionDetector.start();
 
         ////// Config Options
         //motionDetector.setCheckInterval(500);
@@ -55,16 +51,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        if (checkCameraHardware()) {
+        motionDetector.onResume();
+
+        if (motionDetector.checkCameraHardware()) {
             txtStatus.setText("Kamera gefunden");
-
-            mCamera = getCameraInstance();
-
-            // configure preview
-            SurfaceView preview = (SurfaceView) findViewById(R.id.surfaceView);
-            previewHolder = preview.getHolder();
-            previewHolder.addCallback(surfaceCallback);
-            previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
         } else {
             txtStatus.setText("Keine Kamera verfuegbar");
         }
@@ -73,120 +63,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
-        releaseCamera();
-        if (previewHolder != null) previewHolder.removeCallback(surfaceCallback);
-        if (motionDetector != null) motionDetector.stopDetection();
-    }
-
-    private boolean checkCameraHardware() {
-        if (getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
-            // this device has a camera
-            return true;
-        } else {
-            // no camera on this device
-            return false;
-        }
-    }
-
-    public Camera getCameraInstance(){
-        Camera c = null;
-        try {
-            c = Camera.open(); // attempt to get a Camera instance
-        }
-        catch (Exception e){
-            // Camera is not available (in use or does not exist)
-            txtStatus.setText("Kamera nicht zur Benutzung freigegeben");
-        }
-        return c; // returns null if camera is unavailable
-    }
-
-    private void releaseCamera(){
-        if (mCamera != null){
-            mCamera.setPreviewCallback(null);
-            if (inPreview) mCamera.stopPreview();
-            inPreview = false;
-            mCamera.release();        // release the camera for other applications
-            mCamera = null;
-        }
-    }
-
-    private Camera.PreviewCallback previewCallback = new Camera.PreviewCallback() {
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public void onPreviewFrame(byte[] data, Camera cam) {
-            if (data == null) return;
-            Camera.Size size = cam.getParameters().getPreviewSize();
-            if (size == null) return;
-
-            Log.d("MainActivity", "preview yoyoyo="+size.width+"/"+size.height);
-
-            if (motionDetector != null) {
-                motionDetector.consume(data, size.width, size.height);
-            }
-        }
-    };
-
-
-    private SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public void surfaceCreated(SurfaceHolder holder) {
-            try {
-                mCamera.setPreviewDisplay(previewHolder);
-                mCamera.setPreviewCallback(previewCallback);
-            } catch (Throwable t) {
-                Log.e("PreviewDemo-surface", "Exception in setPreviewDisplay()", t);
-            }
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
-            Camera.Parameters parameters = mCamera.getParameters();
-            Camera.Size size = getBestPreviewSize(width, height, parameters);
-            if (size != null) {
-                parameters.setPreviewSize(size.width, size.height);
-                Log.d("MainActivity", "Using width=" + size.width + " height=" + size.height);
-            }
-            mCamera.setParameters(parameters);
-            mCamera.startPreview();
-            inPreview = true;
-        }
-
-        /**
-         * {@inheritDoc}
-         */
-        @Override
-        public void surfaceDestroyed(SurfaceHolder holder) {
-            // Ignore
-        }
-    };
-
-    private static Camera.Size getBestPreviewSize(int width, int height, Camera.Parameters parameters) {
-        Camera.Size result = null;
-
-        for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
-            if (size.width <= width && size.height <= height) {
-                if (result == null) {
-                    result = size;
-                } else {
-                    int resultArea = result.width * result.height;
-                    int newArea = size.width * size.height;
-
-                    if (newArea > resultArea) result = size;
-                }
-            }
-        }
-
-        return result;
+        motionDetector.onPause();
     }
 
 }
--- app/src/main/java/de/appsthatmatter/knxpresso/bewegungsmelder/motiondetection/MotionDetector.java
+++ app/src/main/java/de/appsthatmatter/knxpresso/bewegungsmelder/motiondetection/MotionDetector.java
@@ -1,78 +1,99 @@
 package de.appsthatmatter.knxpresso.bewegungsmelder.motiondetection;
 
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
 import android.os.Handler;
+import android.util.Log;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 
+import de.appsthatmatter.knxpresso.bewegungsmelder.R;
+
 /**
  * Created by jonas on 26.05.16.
  */
-public class MotionDetector extends Thread {
+public class MotionDetector {
+    class MotionDetectorThread extends Thread {
+        private AtomicBoolean isRunning = new AtomicBoolean(true);
+
+        public void stopDetection() {
+            isRunning.set(false);
+        }
+
+        @Override
+        public void run() {
+            while (isRunning.get()) {
+                long now = System.currentTimeMillis();
+                if (now-lastCheck > checkInterval) {
+                    lastCheck = now;
+
+                    if (nextData.get() != null) {
+                        int[] img = ImageProcessing.decodeYUV420SPtoLuma(nextData.get(), nextWidth.get(), nextHeight.get());
+
+                        // check if it is too dark
+                        int lumaSum = 0;
+                        for (int i : img) {
+                            lumaSum += i;
+                        }
+                        if (lumaSum < minLuma) {
+                            if (motionDetectorCallback != null) {
+                                mHandler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        motionDetectorCallback.onTooDark();
+                                    }
+                                });
+                            }
+                        } else if (detector.detect(img, nextWidth.get(), nextHeight.get())) {
+                            // check
+                            if (motionDetectorCallback != null) {
+                                mHandler.post(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        motionDetectorCallback.onMotionDetected();
+                                    }
+                                });
+                            }
+                        }
+                    }
+                }
+                try {
+                    Thread.sleep(10);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
     private final AggregateLumaMotionDetection detector;
     private long checkInterval = 500;
     private long lastCheck = 0;
     private MotionDetectorCallback motionDetectorCallback;
-    private AtomicBoolean isRunning = new AtomicBoolean(true);
     private Handler mHandler = new Handler();
 
     private AtomicReference<byte[]> nextData = new AtomicReference<>();
     private AtomicInteger nextWidth = new AtomicInteger();
     private AtomicInteger nextHeight = new AtomicInteger();
     private int minLuma = 1000;
+    private MotionDetectorThread worker;
 
-    public MotionDetector() {
+    private Camera mCamera;
+    private boolean inPreview;
+    private SurfaceHolder previewHolder;
+    private Context mContext;
+    private SurfaceView mSurface;
+
+    public MotionDetector(Context context, SurfaceView previewSurface) {
         detector = new AggregateLumaMotionDetection();
-    }
-
-    @Override
-    public void run() {
-        while (isRunning.get()) {
-            long now = System.currentTimeMillis();
-            if (now-lastCheck > checkInterval) {
-                lastCheck = now;
-
-                if (nextData.get() != null) {
-                    int[] img = ImageProcessing.decodeYUV420SPtoLuma(nextData.get(), nextWidth.get(), nextHeight.get());
-
-                    // check if it is too dark
-                    int lumaSum = 0;
-                    for (int i : img) {
-                        lumaSum += i;
-                    }
-                    if (lumaSum < minLuma) {
-                        if (motionDetectorCallback != null) {
-                            mHandler.post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    motionDetectorCallback.onTooDark();
-                                }
-                            });
-                        }
-                    } else if (detector.detect(img, nextWidth.get(), nextHeight.get())) {
-                        // check
-                        if (motionDetectorCallback != null) {
-                            mHandler.post(new Runnable() {
-                                @Override
-                                public void run() {
-                                    motionDetectorCallback.onMotionDetected();
-                                }
-                            });
-                        }
-                    }
-                }
-            }
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    public void stopDetection() {
-        isRunning.set(false);
+        mContext = context;
+        mSurface = previewSurface;
     }
 
     public void setMotionDetectorCallback(MotionDetectorCallback motionDetectorCallback) {
@@ -95,5 +116,132 @@ public class MotionDetector extends Thread {
 
     public void setLeniency(int l) {
         detector.setLeniency(l);
+    }
+
+    public void onResume() {
+        if (checkCameraHardware()) {
+            mCamera = getCameraInstance();
+
+            worker = new MotionDetectorThread();
+            worker.start();
+
+            // configure preview
+            previewHolder = mSurface.getHolder();
+            previewHolder.addCallback(surfaceCallback);
+            previewHolder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+        }
+    }
+
+    public boolean checkCameraHardware() {
+        if (mContext.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+
+    private Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+            //txtStatus.setText("Kamera nicht zur Benutzung freigegeben");
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    private Camera.PreviewCallback previewCallback = new Camera.PreviewCallback() {
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void onPreviewFrame(byte[] data, Camera cam) {
+            if (data == null) return;
+            Camera.Size size = cam.getParameters().getPreviewSize();
+            if (size == null) return;
+
+            consume(data, size.width, size.height);
+        }
+    };
+
+
+    private SurfaceHolder.Callback surfaceCallback = new SurfaceHolder.Callback() {
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void surfaceCreated(SurfaceHolder holder) {
+            try {
+                mCamera.setPreviewDisplay(previewHolder);
+                mCamera.setPreviewCallback(previewCallback);
+            } catch (Throwable t) {
+                Log.e("MotionDetector", "Exception in setPreviewDisplay()", t);
+            }
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            Camera.Parameters parameters = mCamera.getParameters();
+            Camera.Size size = getBestPreviewSize(width, height, parameters);
+            if (size != null) {
+                parameters.setPreviewSize(size.width, size.height);
+                Log.d("MotionDetector", "Using width=" + size.width + " height=" + size.height);
+            }
+            mCamera.setParameters(parameters);
+            mCamera.startPreview();
+            inPreview = true;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public void surfaceDestroyed(SurfaceHolder holder) {
+            // Ignore
+        }
+    };
+
+    private static Camera.Size getBestPreviewSize(int width, int height, Camera.Parameters parameters) {
+        Camera.Size result = null;
+
+        for (Camera.Size size : parameters.getSupportedPreviewSizes()) {
+            if (size.width <= width && size.height <= height) {
+                if (result == null) {
+                    result = size;
+                } else {
+                    int resultArea = result.width * result.height;
+                    int newArea = size.width * size.height;
+
+                    if (newArea > resultArea) result = size;
+                }
+            }
+        }
+
+        return result;
+    }
+
+    public void onPause() {
+        releaseCamera();
+        if (previewHolder != null) previewHolder.removeCallback(surfaceCallback);
+        if (worker != null) worker.stopDetection();
+    }
+
+    private void releaseCamera(){
+        if (mCamera != null){
+            mCamera.setPreviewCallback(null);
+            if (inPreview) mCamera.stopPreview();
+            inPreview = false;
+            mCamera.release();        // release the camera for other applications
+            mCamera = null;
+        }
     }
 }
